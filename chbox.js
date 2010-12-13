2aba6ad665b1454b6903e2d080f3b6f0dae08ee9
Denis Sobolev <dns.sobol@gmail.com>
2010-12-13 15:08:45 +0300

    Remove list.patch

--- chbox.js
+++ chbox.js
@@ -19,11 +19,32 @@ if (window.rcmail) {
     rcmail.register_command('plugin.chbox.selectmenu', rcmail_selectmenu, true);
     chbox_menu();
     // add event-listener to message list
-    if (rcmail.message_list)
+    if (rcmail.message_list) {
       rcmail.message_list.addEventListener('select', function(list){
-        //alert('select');
+        $('#messagelist input:checked').removeAttr('checked');
+        var selection = rcmail.message_list ? $.merge([], rcmail.message_list.get_selection()) : [];
+        // exit if no mailbox specified or if selection is empty
+        if (!rcmail.env.uid && !selection.length)
+          return;
+        // also select childs of collapsed rows
+        /*
+        for (var uid, i=0, len=selection.length; i<len; i++) {
+          uid = selection[i];
+          if (rcmail.message_list.rows[uid].has_children && !rcmail.message_list.rows[uid].expanded)
+            rcmail.message_list.select_childs(uid);
+        }
+        var selection = rcmail.message_list ? $.merge([], rcmail.message_list.get_selection()) : [];
+        */
+        for (var uid, i=0, len=selection.length; i<len; i++) {
+            uid = selection[i];
+            var select = document.getElementById('rcmselect'+uid);
+            if (select) select.checked = true;
+        }
       });
+    }
   });
+
+
   rcmail.addEventListener('listupdate','chbox_menu');
   rcmail.addEventListener('insertrow', function(evt) {
     var row = evt.row
